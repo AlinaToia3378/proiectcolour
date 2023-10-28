c150e4e5312f38a77f15c28e4923ca81f1bf5954
AlinaToia3378 <a.toia2002@mail.ru>
2023-10-28 09:27:39 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', (event) => {
+   console.log(event.code)
+})
+
+function gerenerateRandomColor() {
+    // RGB
+    // #FF0000
+    // #00FF00
+    // #0000FF
+
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+     color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+function setRandomColors() {
+    cols.forEach(col => {
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = chroma.random()
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors()
